d6987f084f9a7739d0b58b5b4644b584dbf91115
heruijie <heruijie@stu.scu.edu.cn>
2025-09-25 10:05:42 +0000

    create envirenment

---
+++ Code/gtest/main.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+/*
+    简单使用gtest框架
+*/
+
+auto main()
+-> int
+{
+    /*
+        1. 定义测试用例
+        2. 初始化测试环境
+        3. 运行测试用例
+    */
+    return 0;
+}
